61ff17e9c8825ed70bc8ab369eaffa946a833b7a
DannyPoizot <poizot.dan@gmail.com>
2022-01-21 12:31:53 +0100

    mailbox

--- affichage-conditionnel/src/index.js
+++ affichage-conditionnel/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginControl from './components/LoginControl';
+import Mailbox from './components/Mailbox';
 import './index.css';
 
 
 
-
+const messages = ["React", "Re: React", "Re:Re React"]
 ReactDOM.render(
   <>
    <LoginControl />
+   <Mailbox unreadMessages={messages} />
   </>,
   document.getElementById('root')
 );
